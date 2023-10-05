ac5bac81aaac2dc2ec13c2dcff19277f2fe0abe7
sandikodev <androxoss@hotmail.com>
2023-10-06 04:11:53 +0700

    create angkringan pages

---
+++ src/routes/angkringan/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+import { Pages } from "~/components/layouts/Pages";
+
+export default component$(() => {
+  return (
+    <Pages>
+        jadwal Ngangkring bos. dikerjakan nantiii
+    </Pages>
+  )
+});
